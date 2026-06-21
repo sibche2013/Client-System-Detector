ba114e141abdeb83ad1cf71acd126fb248c6f2e0
Amin Arjmand <aminarj2000@gmail.com>
2026-06-21 14:59:06 +0330

    Update Browser&OsDetection.js

--- Browser&OsDetection.js
+++ Browser&OsDetection.js
@@ -1,173 +1,160 @@
-    /*
-     * This File Was Coded By Amin Arjmand & viazenetti GmbH (Christian Ludwig) | Email: aminarj2000@gmail.com | Site: aminarjmand.com | Github Profile : https://github.com/sibche2013
-     */
-    (function (window) {
-        {
-            var unknown = '-';
+/* Developed by Amin Arjmand & viazenetti GmbH (Christian Ludwig)
+Email: aminarj2000@gmail.com | Site: aminarjmand.com | GitHub: @sibche2013 
+*/
 
-            // Get Screen Resolution
-            var screenSize = screen.width + "*" + screen.height;
+document.addEventListener("DOMContentLoaded", async function () {
+    const unknown = '-';
 
-            // Get Browser Name & Version
-            var navigatorVersion = navigator.appVersion;
-            var navigatorUserAgent = navigator.userAgent;
-            var browserName = navigator.appName;
-            var browserVersion = navigator.appVersion;
-            var nameOffset, verOffset, ix;
+    // 1. Screen Size Info
+    const screenSize = window.screen.width + " × " + window.screen.height + " Pixels";
+    document.getElementById("screen-info").textContent = screenSize;
 
-            // Old Opera Browser
-            if ((verOffset = navigatorUserAgent.indexOf('Opera')) != -1) {
-                browserName = 'Opera';
-                browserVersion = navigatorUserAgent.substring(verOffset + 6);
-                if ((verOffset = navigatorUserAgent.indexOf('Version')) != -1) {
-                    browserVersion = navigatorUserAgent.substring(verOffset + 8);
-                }
-            }
+    // 2. Network Connectivity Status
+    const updateNetworkStatus = () => {
+        document.getElementById("network-info").textContent = navigator.onLine ? "Online (Connected)" : "Offline (No Internet)";
+    };
+    window.addEventListener('online', updateNetworkStatus);
+    window.addEventListener('offline', updateNetworkStatus);
+    updateNetworkStatus();
 
-            // New Opera Browser
-            if ((verOffset = navigatorUserAgent.indexOf('OPR')) != -1) {
-                browserName = 'Opera';
-                browserVersion = navigatorUserAgent.substring(verOffset + 4);
-            }
-
-            // Edge Browser
-            else if ((verOffset = navigatorUserAgent.indexOf('Edge')) != -1) {
-                browserName = 'Microsoft Edge';
-                browserVersion = navigatorUserAgent.substring(verOffset + 5);
-            }
-
-            // Old MSIE Browser
-            else if ((verOffset = navigatorUserAgent.indexOf('MSIE')) != -1) {
-                browserName = 'Microsoft Internet Explorer';
-                browserVersion = navigatorUserAgent.substring(verOffset + 5);
-            }
-
-            // New MSIE Browser (11+)
-            else if (navigatorUserAgent.indexOf('Trident/') != -1) {
-                browserName = 'Microsoft Internet Explorer';
-                browserVersion = navigatorUserAgent.substring(navigatorUserAgent.indexOf('rv:') + 3);
-            }
-
-            // Chrome Browser
-            else if ((verOffset = navigatorUserAgent.indexOf('Chrome')) != -1) {
-                browserName = 'Chrome';
-                browserVersion = navigatorUserAgent.substring(verOffset + 7);
-            }
-
-            // Safari Browser
-            else if ((verOffset = navigatorUserAgent.indexOf('Safari')) != -1) {
-                browserName = 'Safari';
-                browserVersion = navigatorUserAgent.substring(verOffset + 7);
-                if ((verOffset = navigatorUserAgent.indexOf('Version')) != -1) {
-                    browserVersion = navigatorUserAgent.substring(verOffset + 8);
-                }
-            }
-
-            // Firefox Browser
-            else if ((verOffset = navigatorUserAgent.indexOf('Firefox')) != -1) {
-                browserName = 'Firefox';
-                browserVersion = navigatorUserAgent.substring(verOffset + 8);
-            }
-
-            // Other Browsers
-            else if ((nameOffset = navigatorUserAgent.lastIndexOf(' ') + 1) < (verOffset = navigatorUserAgent.lastIndexOf('/'))) {
-                browserName = navigatorUserAgent.substring(nameOffset, verOffset);
-                browserVersion = navigatorUserAgent.substring(verOffset + 1);
-                if (browserName.toLowerCase() == browserName.toUpperCase()) {
-                    browserName = navigator.appName;
-                }
-            }
-
-            // Trim The Version String
-            if ((ix = browserVersion.indexOf(';')) != -1) browserVersion = browserVersion.substring(0, ix);
-            if ((ix = browserVersion.indexOf(' ')) != -1) browserVersion = browserVersion.substring(0, ix);
-            if ((ix = browserVersion.indexOf(')')) != -1) browserVersion = browserVersion.substring(0, ix);
-
-            // Mobile Browser Or Desktop ???
-            mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(navigatorVersion);
-
-            // Get OS Name
-            var os = unknown;
-            var clientStrings = [
-                {s: 'Windows 10', r: /(Windows 10.0|Windows NT 10.0)/},
-                {s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/},
-                {s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/},
-                {s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/},
-                {s: 'Windows Vista', r: /Windows NT 6.0/},
-                {s: 'Windows Server 2003', r: /Windows NT 5.2/},
-                {s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/},
-                {s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/},
-                {s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/},
-                {s: 'Windows 98', r: /(Windows 98|Win98)/},
-                {s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/},
-                {s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
-                {s: 'Windows CE', r: /Windows CE/},
-                {s: 'Windows 3.11', r: /Win16/},
-                {s: 'Android', r: /Android/},
-                {s: 'Open BSD', r: /OpenBSD/},
-                {s: 'Sun OS', r: /SunOS/},
-                {s: 'Linux', r: /(Linux|X11)/},
-                {s: 'iOS', r: /(iPhone|iPad|iPod)/},
-                {s: 'Mac OS X', r: /Mac OS X/},
-                {s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
-                {s: 'QNX', r: /QNX/},
-                {s: 'UNIX', r: /UNIX/},
-                {s: 'BeOS', r: /BeOS/},
-                {s: 'OS/2', r: /OS\/2/},
-                {s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
-            ];
-            for (var id in clientStrings) {
-                var cs = clientStrings[id];
-                if (cs.r.test(navigatorUserAgent)) {
-                    os = cs.s;
-                    break;
-                }
-            }
-
-            var osVersion = unknown;
-
-            if (/Windows/.test(os)) {
-                osVersion = /Windows (.*)/.exec(os)[1];
-                os = 'Windows';
-            }
-
-            switch (os) {
-                case 'Mac OS X':
-                    osVersion = /Mac OS X (10[\.\_\d]+)/.exec(navigatorUserAgent)[1];
-                    break;
-
-                case 'Android':
-                    osVersion = /Android ([\.\_\d]+)/.exec(navigatorUserAgent)[1];
-                    break;
-
-                case 'iOS':
-                    osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(navigatorVersion);
-                    osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
-                    break;
-            }
-
-
+    // 3. Battery API Setup
+    if (navigator.getBattery) {
+        try {
+            const battery = await navigator.getBattery();
+            const updateBatteryDisplay = () => {
+                const percentage = Math.round(battery.level * 100) + "%";
+                const status = battery.charging ? " (Charging)" : " (Discharging)";
+                document.getElementById("battery-info").textContent = percentage + status;
+            };
+            battery.addEventListener('levelchange', updateBatteryDisplay);
+            battery.addEventListener('chargingchange', updateBatteryDisplay);
+            updateBatteryDisplay();
+        } catch (e) {
+            document.getElementById("battery-info").textContent = "Not Supported / Blocked";
         }
+    } else {
+        document.getElementById("battery-info").textContent = "Not Supported by Browser";
+    }
 
-        window.info = {
-            os: os,
-            osVersion: osVersion,
-            screen: screenSize,
-            browser: browserName,
-            browserVersion: browserVersion,
-            mobile: mobile
-        };
-    }(this));
+    // 4. Browser / Agent Parsing Logic
+    const userAgent = navigator.userAgent;
+    let browserName = navigator.appName;
+    let browserVersion = "" + parseFloat(navigator.appVersion);
+    let verOffset, nameOffset, ix;
 
-    document.getElementById("systemInformation").innerHTML = "<strong>Your System Information :</strong><br>Your Windows Name Is: <strong>" + info.os + " " + info.osVersion + "</strong><br>Your Monitor Screen Size Is: <strong>" + info.screen + "</strong><br>Your Browser Name & Version Is: <strong>" + info.browser + " " + info.browserVersion + "</strong>";
-
-    //Check That User Browser Is Dektop Or Mobile?
-    function mobileDesktop() {
-        var x = document.getElementById("mobileDesktop");
-        if (mobile) {
-            x.innerHTML = "Your Are Viewing This Page By Your <strong>Mobile Browser </strong>!";
-        } else {
-            x.innerHTML = "You Are Viewing This Page By <strong>Desktop Browser </strong>!";
+    if ((verOffset = userAgent.indexOf("Opera")) !== -1) {
+        browserName = "Opera";
+        browserVersion = userAgent.substring(verOffset + 6);
+        if ((verOffset = userAgent.indexOf("Version")) !== -1) {
+            browserVersion = userAgent.substring(verOffset + 8);
+        }
+    } else if ((verOffset = userAgent.indexOf("OPR")) !== -1) {
+        browserName = "Opera";
+        browserVersion = userAgent.substring(verOffset + 4);
+    } else if ((verOffset = userAgent.indexOf("Edg")) !== -1) {
+        browserName = "Microsoft Edge";
+        browserVersion = userAgent.substring(verOffset + 4);
+    } else if ((verOffset = userAgent.indexOf("MSIE")) !== -1) {
+        browserName = "Microsoft Internet Explorer";
+        browserVersion = userAgent.substring(verOffset + 5);
+    } else if (userAgent.indexOf("Trident/") !== -1) {
+        browserName = "Microsoft Internet Explorer";
+        browserVersion = userAgent.substring(userAgent.indexOf("rv:") + 3);
+    } else if ((verOffset = userAgent.indexOf("Chrome")) !== -1) {
+        browserName = "Chrome";
+        browserVersion = userAgent.substring(verOffset + 7);
+    } else if ((verOffset = userAgent.indexOf("Safari")) !== -1) {
+        browserName = "Safari";
+        browserVersion = userAgent.substring(verOffset + 7);
+        if ((verOffset = userAgent.indexOf("Version")) !== -1) {
+            browserVersion = userAgent.substring(verOffset + 8);
+        }
+    } else if ((verOffset = userAgent.indexOf("Firefox")) !== -1) {
+        browserName = "Firefox";
+        browserVersion = userAgent.substring(verOffset + 8);
+    } else if ((nameOffset = userAgent.lastIndexOf(' ') + 1) < (verOffset = userAgent.lastIndexOf('/'))) {
+        browserName = userAgent.substring(nameOffset, verOffset);
+        browserVersion = userAgent.substring(verOffset + 1);
+        if (browserName.toLowerCase() === browserName.toUpperCase()) {
+            browserName = navigator.appName;
         }
     }
-    mobileDesktop();
+
+    // Clean up version string
+    if ((ix = browserVersion.indexOf(";")) !== -1) browserVersion = browserVersion.substring(0, ix);
+    if ((ix = browserVersion.indexOf(" ")) !== -1) browserVersion = browserVersion.substring(0, ix);
+    if ((ix = browserVersion.indexOf(")")) !== -1) browserVersion = browserVersion.substring(0, ix);
+
+    document.getElementById("browser-info").textContent = browserName + " (v" + browserVersion + ")";
+
+    // 5. Native OS Matching
+    let os = unknown;
+    const clientStrings = [
+        {s: 'Windows 10 / 11', r: /(Windows 10.0|Windows NT 10.0)/},
+        {s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/},
+        {s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/},
+        {s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/},
+        {s: 'Android', r: /Android/},
+        {s: 'Linux', r: /(Linux|X11)/},
+        {s: 'iOS', r: /(iPhone|iPad|iPod)/},
+        {s: 'Mac OS X', r: /Mac OS X/},
+        {s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/}
+    ];
+
+    for (let id in clientStrings) {
+        let cs = clientStrings[id];
+        if (cs.r.test(userAgent)) {
+            os = cs.s;
+            break;
+        }
+    }
+
+    let osVersion = unknown;
+    if (/Windows/.test(os)) {
+        osVersion = "NT Framework";
+        os = 'Windows';
+    }
+
+    // Modern Windows 11 High-Accuracy Fallback API
+    if (navigator.userAgentData && navigator.userAgentData.getHighEntropyValues) {
+        try {
+            const uaHints = await navigator.userAgentData.getHighEntropyValues(["platformVersion"]);
+            if (navigator.userAgentData.platform === "Windows") {
+                const majorPlatformVersion = parseInt(uaHints.platformVersion.split('.')[0]);
+                if (majorPlatformVersion >= 13) {
+                    os = "Windows";
+                    osVersion = "11 (Modern Platform)";
+                } else {
+                    os = "Windows";
+                    osVersion = "10";
+                }
+            }
+        } catch (e) {}
+    }
+
+    if (osVersion === unknown || osVersion === "NT Framework") {
+        if (/Mac OS X/.test(userAgent)) {
+            const match = /Mac OS X (10[\.\_\d]+)/.exec(userAgent);
+            if (match) osVersion = match[1].replace(/_/g, '.');
+        } else if (/Android/.test(userAgent)) {
+            const match = /Android ([\.\_\d]+)/.exec(userAgent);
+            if (match) osVersion = match[1];
+        } else if (/iPhone|iPad|iPod/.test(userAgent)) {
+            const match = /OS (\d+)_(\d+)_?(\d+)?/.exec(navigator.appVersion);
+            if (match) osVersion = match[1] + '.' + match[2] + '.' + (match[3] || '0');
+        }
+    }
+
+    document.getElementById("os-info").textContent = os + " " + (osVersion !== unknown ? osVersion : "");
+
+    // 6. Device Type Evaluation Badge
+    const isMobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(navigator.appVersion) || (navigator.userAgentData && navigator.userAgentData.mobile);
+    const badge = document.getElementById("device-badge");
+    if (isMobile) {
+        badge.textContent = "📱 Mobile Browser Environment";
+        badge.style.background = "#e67e22";
+    } else {
+        badge.textContent = "💻 Desktop Browser Environment";
+        badge.style.background = "#2ecc71";
+    }
+});
